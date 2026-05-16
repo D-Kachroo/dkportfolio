be0c3f870180378de5be432b4f243d2c0c755ec0
David Kachroo <david.kachroo@gmail.com>
2026-05-16 19:55:23 -0400

    Update indigo color value in App.js

--- src/App.js
+++ src/App.js
@@ -143,7 +143,7 @@ export default function App() {
     blue:  '#1d4ed8',
     red:   '#db0202',     
     teal:  '#08ceb7',
-    indigo:'#2563eb'       
+    indigo:'#1e40af'       
   };
 
   const getColorClasses = (color) => {
